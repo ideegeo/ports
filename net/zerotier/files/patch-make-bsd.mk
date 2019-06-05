f6f2c2b009a7b4e618ee2a0cf23de16b380431d7
Aunt Sybil <ansible@koan-ci.com>
2019-06-05 12:58:12 +0000

    Revert "net/zerotier: move to v1.2.12-253"
    This reverts commit e1ed57fdacc66dbf5012ea6167d6855b8e9c843a because
    it BREAKS ALL THE THINGS.

--- net/zerotier/files/patch-make-bsd.mk
+++ net/zerotier/files/patch-make-bsd.mk
@@ -1,11 +1,11 @@
---- make-bsd.mk.orig	2019-06-04 20:24:02 UTC
+--- make-bsd.mk.orig	2018-09-13 10:12:22 UTC
 +++ make-bsd.mk
-@@ -13,7 +13,7 @@ ifeq ($(ZT_SANITIZE),1)
- endif
- # "make debug" is a shortcut for this
- ifeq ($(ZT_DEBUG),1)
--	CFLAGS+=-Wall -Werror -g -pthread $(INCLUDES) $(DEFS)
-+	CFLAGS+=-Wall -Werror -Wno-unused-private-field -g -pthread $(INCLUDES) $(DEFS)
- 	LDFLAGS+=
- 	STRIP=echo
- 	ZT_TRACE=1
+@@ -159,7 +159,7 @@ clean:
+ 	rm -rf *.a *.o node/*.o controller/*.o osdep/*.o service/*.o ext/http-parser/*.o build-* zerotier-one zerotier-idtool zerotier-selftest zerotier-cli $(ONE_OBJS) $(CORE_OBJS)
+ 
+ debug:	FORCE
+-	gmake -j 4 ZT_DEBUG=1
++	$(MAKE) -j 4 ZT_DEBUG=1
+ 
+ install:	one
+ 	rm -f /usr/local/sbin/zerotier-one
